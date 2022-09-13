ababc99e8ec9e0717dbadbde7b536fbaad14e686
Reja Jamil <reja.copyright@gmail.com>
2022-09-14 00:30:56 +0700

    fix: migration add status

--- database/migrations/2022_09_13_020345_create_home_banners_table.php
+++ database/migrations/2022_09_13_020345_create_home_banners_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->string('file')->nullable();
+            $table->integer('status')->nullable()->default(1);
             $table->softDeletes();
             $table->timestamps();
         });
